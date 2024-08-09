8b541351b7837b695eb7f2afa197cf98d95b0c45
mohamedshahin244 <mohamedhamdy911@icloud.com>
2024-08-09 15:57:36 +0300

    first

---
+++ css/java.js
@@ -0,0 +1,11 @@
+// var mainListDiv = document.getElementById("container"),
+//     mediaButton = document.getElementById("hamburger");
+
+// mediaButton.onclick = function () {
+    
+//     "use strict";
+    
+//     mainListDiv.classList.toggle("show_list");
+//     mediaButton.classList.toggle("active");
+    
+// };
